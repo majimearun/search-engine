e159ce7631656fd369239a5d2d20006dc4a76e51
majimearun <arunachala0619@gmail.com>
2023-03-02 14:59:11 +0530

    improved spell check by giving importance to swap

--- src/query_functions.py
+++ src/query_functions.py
@@ -318,12 +318,13 @@ def boolean_filter(
         return phrase_query(queries, bi_word_index, perm_index, rev_perm_index)
 
 
-def levenshtein_distance(s1: str, s2: str):
+def levenshtein_distance(s1: str, s2: str, swapping_importance: bool = True):
     """Calculates the levenshtein distance between two strings
 
     Args:
         s1 (str): first string
         s2 (str): second string
+        swapping_importance (bool, optional): Whether swapping two characters is more important than deleting or inserting a character. Defaults to True.
 
     Returns:
         int: levenshtein distance between the two strings
@@ -333,20 +334,45 @@ def levenshtein_distance(s1: str, s2: str):
         levenshtein[i, 0] = i
     for j in range(len(s2) + 1):
         levenshtein[0, j] = j
-    for i in range(1, len(s1) + 1):
-        for j in range(1, len(s2) + 1):
-            if s1[i - 1] == s2[j - 1]:
-                levenshtein[i, j] = levenshtein[i - 1, j - 1]
-            else:
-                levenshtein[i, j] = (
-                    min(
-                        levenshtein[i - 1, j - 1],
-                        levenshtein[i - 1, j],
-                        levenshtein[i, j - 1],
+    if not swapping_importance:
+        for i in range(1, len(s1) + 1):
+            for j in range(1, len(s2) + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    levenshtein[i, j] = levenshtein[i - 1, j - 1]
+                else:
+                    levenshtein[i, j] = (
+                        min(
+                            levenshtein[i - 1, j - 1],
+                            levenshtein[i - 1, j],
+                            levenshtein[i, j - 1],
+                        )
+                        + 1
                     )
-                    + 1
-                )
-    return levenshtein[len(s1), len(s2)]
+        return levenshtein[len(s1), len(s2)]
+    else:
+        for i in range(1, len(s1) + 1):
+            for j in range(1, len(s2) + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    levenshtein[i, j] = levenshtein[i - 1, j - 1]
+                else:
+                    levenshtein[i, j] = (
+                        min(
+                            levenshtein[i - 1, j - 1],
+                            levenshtein[i - 1, j],
+                            levenshtein[i, j - 1],
+                        )
+                        + 1
+                    )
+                    if (
+                        i > 1
+                        and j > 1
+                        and s1[i - 1] == s2[j - 2]
+                        and s1[i - 2] == s2[j - 1]
+                    ):
+                        levenshtein[i, j] = min(
+                            levenshtein[i, j], levenshtein[i - 2, j - 2] + 1
+                        )
+        return levenshtein[len(s1), len(s2)]
 
 
 def spell_check_query(query: str, inverted_list: dict[str, LinkedList]):
